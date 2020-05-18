b2b39920637eab7a47162739a3f58704d7c418c2
saghal <mohammadsgh98@gmail.com>
2020-05-18 19:22:19 +0430

    add second versions to lab 5

--- Lab-05/Lab-05.sql
+++ Lab-05/Lab-05.sql
@@ -17,6 +17,39 @@ PIVOT
 COUNT(OrderQty)
 for [Group] IN (Europe, [North America], Pacific) 
 )as PVT
+
+
+
+-- question 1 (version 2)
+WITH productToSalesOrderDetail(ProductID, Name,OrderQty, SalesOrderID)as(
+SELECT P.ProductID, P.Name,SOD.OrderQty, SOD.SalesOrderID from Sales.SalesOrderDetail as SOD INNER JOIN Production.Product as P ON
+SOD.ProductID = P.ProductID),
+
+PSODtoSalesTerritory(ProductID, Name,OrderQty, TerritoryID) as(
+SELECT PSOD.ProductID,PSOD.Name, PSOD.OrderQty , SOH.TerritoryID FROM productToSalesOrderDetail as PSOD INNER JOIN Sales.SalesOrderHeader as SOH 
+ON PSOD.SalesOrderID = SOH.SalesOrderID
+),
+
+table1(ProductID, Name, OrderQty, Region) as(
+SELECT PSODtoST.ProductID, PSODtoST.Name, PSODtoST.OrderQty, ST.[Group] FROM PSODtoSalesTerritory as PSODtoST INNER JOIN Sales.SalesTerritory as ST
+ON PSODtoST.TerritoryID = ST.TerritoryID
+),
+
+table2(Name, Europe, Pacific, NorthAmerica) as (
+SELECT Name, Europe, Pacific, [North America]
+FROM (select * FROM table1 ) as m
+PIVOT(
+    COUNT(ProductID)
+    FOR Region IN(Europe,Pacific,[North America]))as PVT
+)
+
+SELECT Name, SUM(Europe) as Europe, SUM(NorthAmerica) as [North America], SUM(Pacific) AS Pacific
+FROM table2 
+GROUP BY Name
+ORDER BY Name
+
+
+
  
 -- question 2
 
@@ -106,6 +139,54 @@ select dbo.F_date('2019/13/17') as date
 select dbo.F_date('14/12/2019') as date
 
 drop FUNCTION F_date
+
+
+
+-- question 4 (version 2)
+Create dbo.MonthConverter (@input VARCHAR(2))
+RETURNS VARCHAR(20)
+AS BEGIN
+    DECLARE @ret VARCHAR(20)
+
+    SET @ret =( select case
+                        when @input = '01' then  'January'
+                        when @input = '02' then  'February'
+                        when @input = '03' then  'March'
+                        When @input = '04' then  'April'
+                        When @input = '05' then  'May'
+                        when @input = '06' then  'June'
+                        when @input = '07' then  'July'
+                        when @input = '08' then  'August'
+                        when @input = '09' then  'September'
+                        when @input = '10' then  'October'
+                        when @input = '11' then  'November'
+                        when @input = '12' then  'December'
+                        else 'Wrong month'
+                        end)
+
+
+    RETURN @ret
+END
+
+
+Create FUNCTION dbo.DateConverter (@input VARCHAR(10))
+RETURNS VARCHAR(50)
+AS BEGIN
+    DECLARE @ret VARCHAR(50)
+    SET @ret =( select case
+                        when @input is null then 'Wrong DateTime'
+                        when LEN(@input) <> 10 then 'Wrong DateTime'
+                        when(select dbo.MonthConverter(SUBSTRING(@input,6,2)))= 'Wrong month' then 'Wrong DateTime'
+                        when SUBSTRING(@input,5,1) = '/' and SUBSTRING(@input,8,1)='/' then
+                             SUBSTRING(@input,9,2) + ' ' + (select dbo.MonthConverter(SUBSTRING(@input,6,2))) + ' month ' + SUBSTRING(@input,1,4)
+                        else 'Wrong DateTime'
+                    end)
+    RETURN @ret
+END
+
+
+SELECT dbo.DateConverter('2020/1/1')
+
 
 -- question 5
 
